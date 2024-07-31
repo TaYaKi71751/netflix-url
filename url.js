ff65915a85dc6657f419ad7e89abb4f212df3334
TaYaKi71751 <shantian.tw@gmail.com>
2024-08-01 05:01:11 +0900

    Add /title/*

--- url.js
+++ url.js
@@ -1,16 +1,30 @@
 
 (async () => {
 	const setUrl = async () => {
-		if(!document.location.pathname.startsWith('/browse')) return;
-		let jbv = undefined;
-		// https://stackoverflow.com/questions/824349/how-do-i-modify-the-url-without-reloading-the-page
-		(new URL(document.location)).searchParams.forEach((value,key) => {
-			if(key == 'jbv'){
-				jbv = value;
-			}
-		});
-		if(jbv == undefined || !jbv.length) return;
-		window.history.pushState('', '', `/us/title/${jbv}`);
+		if(
+			!document.location.pathname.startsWith('/browse') &&
+		 !document.location.pathname.startsWith('/title')
+		) return;
+		if(document.location.pathname.startsWith('/browse')){
+			let jbv = undefined;
+			// https://stackoverflow.com/questions/824349/how-do-i-modify-the-url-without-reloading-the-page
+			(new URL(document.location)).searchParams.forEach((value,key) => {
+				if(key == 'jbv'){
+					jbv = value;
+				}
+			});
+			if(jbv == undefined || !jbv.length) return;
+			window.history.pushState('', '', `/us/title/${jbv}`);
+		}
+		if(document.location.pathname.startsWith('/title')){
+			let jbv = undefined;
+			document.location.pathname.split('/').forEach((str) => {
+				if(str.length && (str.match(/^[0-9]+$/)[0] == str)){
+					jbv = str;
+				}
+			});
+			window.history.pushState('', '', `/us/title/${jbv}`);
+		}
 	};
 			const events = Object.keys(document.__proto__.__proto__).reduce((arr, event)=> { // https://stackoverflow.com/questions/39963850/how-can-i-add-an-event-listener-for-all-events-in-javascript-without-listing-the
 				if(event.startsWith('on')) return [...arr, event.substr(2)];
